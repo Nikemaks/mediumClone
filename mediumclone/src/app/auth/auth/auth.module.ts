3d77a5e45fff190ded454af25e536c0527c7a0de
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-09-21 16:24:51 +0300

    add form

--- mediumclone/src/app/auth/auth/auth.module.ts
+++ mediumclone/src/app/auth/auth/auth.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RegisterComponent} from './comonents/register/register.component';
 import {RouterModule, Routes} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 const rotes: Routes = [
@@ -16,7 +17,8 @@ const rotes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(rotes)
+    RouterModule.forChild(rotes),
+    ReactiveFormsModule
   ]
 })
 export class AuthModule {
--- mediumclone/src/app/auth/auth/comonents/register/register.component.ts
+++ mediumclone/src/app/auth/auth/comonents/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log(this.form.value)
+  }
 }
